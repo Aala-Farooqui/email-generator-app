4fb2c71f833a19ebc4870b1732c33b4224024d7b
Aala Farooqui <aalafarooqui45@gmail.com>
2025-12-28 15:57:41 +0530

    Initial commit - Email Generator full stack app

---
+++ backend/src/main/java/com/email_writer/EmailWriterShApplication.java
@@ -0,0 +1,13 @@
+package com.email_writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailWriterShApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailWriterShApplication.class, args);
+    }
+
+}
